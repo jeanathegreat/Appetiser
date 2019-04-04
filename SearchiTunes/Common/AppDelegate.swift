2e0f505984180512c17167880a86a18bcc878241
Jeana Talle <jeanathegreat@gmail.com>
2019-04-04 12:49:14 +0800

    Added functions needed for app restoration via UIKit's app restoration features

--- SearchiTunes/Common/AppDelegate.swift
+++ SearchiTunes/Common/AppDelegate.swift
@@ -19,28 +19,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    // MARK: State restoration
+    // Was gonna use UIKit's app restoration capability with NSCoding
+    // Decided that this approach would be better to implement here as this is not a data heavy application and no security concerns (no password, no in-app purchases, etc.)
+    // For more complex databases/objects Core Data or even other more complex persistence technologies could be used
+    // Ran out of time to learn how to implement it but I will learn to implement this even after the deadline
+    func application(_ application: UIApplication, shouldSaveApplicationState coder: NSCoder) -> Bool {
+        return true
     }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    func application(_ application: UIApplication, shouldRestoreApplicationState coder: NSCoder) -> Bool {
+        return true
     }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
 
 }
 
